889e0fb9e5cd92f59e2b6b9998ad6645a6f4fe78
Emily Carpenter <ecarpenter004@regis.edu>
2024-01-23 02:03:47 +0000

    added stopwords

--- mapper.py
+++ mapper.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# Regis University 24 Spring 8w1 MSDS 610/S40
+# Emily Carpenter, Week 2, Assignment 2, Jan 22-28, 2024
+
 import sys
 
 # get all lines from stdin
@@ -9,6 +13,13 @@ for line in sys.stdin:
     # split the line into words; splits on any whitespace
     words = line.split()
 
+    # added for week 2 assignment
+    stops = set([
+        'the', 'and', 'to', 'of', 'a', 'in', 'is', 'it', 
+        '.', ',', ':', ';', '-', '\'', '\"', '?', '!'
+        ])
+
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stops:
+            print '%s\t%s' % (word, "1")
